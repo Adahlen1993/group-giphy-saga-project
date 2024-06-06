21561f47b5f44e39344ee0f49c9912cd113635ae
AndrewZajac <andrewzajac05@gmail.com>
2024-06-06 16:01:17 -0500

    added new compenent file

---
+++ src/components/FavoriteGifs/FavoriteGifs.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+export default function FavoriteGifs (){
+
+    return (
+        <></>
+    )
+}
